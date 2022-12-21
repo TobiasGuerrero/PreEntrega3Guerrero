c6ad63438493b091e6cb925c33ecac2c8843e982
Tobias Guerrero <maximoguerrero26@gmail.com>
2022-12-20 22:59:26 -0300

    Se agregó la X al menú hamburguesa activo.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Button = document.querySelector(".header__button");
 const nav = document.querySelector(".nav");
+const button__x = document.querySelector(".button__x");
+const button__menu = document.querySelector(".button__menu");
 
 Button.addEventListener("click", () => {
   nav.classList.toggle("activo");
+  button__x.classList.toggle("activo");
+  button__menu.classList.toggle("activo");
 });
